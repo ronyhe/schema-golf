664066c947090a658d22ea4b943746046f1ddd81
Rony Herut <ronyheruth@wix.com>
2020-07-05 01:25:36 +0300

    Updates

---
+++ precache-manifest.6fa3855cd3afdc40b872acc4a229d03d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6d446b872cbb7358cebce20ceb6cdd8",
+    "url": "/schema-golf/index.html"
+  },
+  {
+    "revision": "6c6a7937c6ef11a89074",
+    "url": "/schema-golf/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "284fae2b88c335b8b22f",
+    "url": "/schema-golf/static/js/2.448d8388.chunk.js"
+  },
+  {
+    "revision": "a6d5bab58f82b51182f890af1f1a3df8",
+    "url": "/schema-golf/static/js/2.448d8388.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c6a7937c6ef11a89074",
+    "url": "/schema-golf/static/js/main.42a88d1e.chunk.js"
+  },
+  {
+    "revision": "7365a7a45967d8529864",
+    "url": "/schema-golf/static/js/runtime-main.4c2e2f35.js"
+  }
+]);
