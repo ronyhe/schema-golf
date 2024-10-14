85c24e117bcbf4198c5b8322c076fc8c947fb7a6
ronyhe <ronyheruth@wix.com>
2024-10-14 23:28:12 +0300

    Remove cruft

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -5,13 +5,11 @@ import Typography from '#mui/Typography'
 import Button from '#mui/Button'
 import AcUnitIcon from '#mui-icons/AcUnit'
 import { AnalogClock } from '#comps/AnalogClock'
-import { Circle } from '#comps/Circle'
 
 export function Main() {
     return (
         <Box>
             <AnalogClock />
-            <Circle />
             <Typography variant='h1'>Hello, world!</Typography>
             <Typography variant='h2'>2 + 2 = {add(2, 2)}</Typography>
             <Button variant='contained' color='primary'>
