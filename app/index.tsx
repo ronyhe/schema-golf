f7f08454e9208fef8b4ae2777a468545c18f5cfd
ronyhe <ronyheruth@wix.com>
2024-10-14 23:33:45 +0300

    Revive examples

--- app/index.tsx
+++ app/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Main } from '#comps/Main'
 import { createRoot } from 'react-dom/client'
 import { CssBaseline } from '@mui/material'
+import examples from './examples.json'
 import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
@@ -10,6 +11,7 @@ import '@fontsource/roboto/700.css'
 document.addEventListener('DOMContentLoaded', main)
 
 async function main() {
+    console.log(examples)
     const root = createRoot(document.querySelector('#app')!)
     root.render(
         <CssBaseline>
