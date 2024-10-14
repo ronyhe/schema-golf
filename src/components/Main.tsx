615d95d7ddbfc2f7aba221f744b9e1b7cc795326
ronyhe <ronyheruth@wix.com>
2024-10-14 21:34:18 +0300

    Install MUI

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { add } from '#logic/math'
+import { CssBaseline } from '@mui/material'
 
 export function Main() {
     return (
-        <div>
-            <h1>Hello, world!</h1>
-            <h2>2 + 2 = {add(2, 2)}</h2>
-        </div>
+        <CssBaseline>
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>2 + 2 = {add(2, 2)}</h2>
+            </div>
+        </CssBaseline>
     )
 }
