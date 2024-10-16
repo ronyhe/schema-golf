67810e4ac812daf4b5e6dc07788d3e27c57c2f19
ronyhe <ronyheruth@wix.com>
2024-10-17 01:39:34 +0300

    Setup resource fab

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -9,8 +9,7 @@ import Tabs from '#mui/Tabs'
 import Tab from '#mui/Tab'
 import AppBar from '#mui/AppBar'
 import Stack from '#mui/Stack'
-import Card from '#mui/Card'
-import Link from '#mui/Link'
+import { ResourcesFab } from '#comps/ResourcesFab'
 
 export interface Level {
     valid: unknown[]
@@ -49,12 +48,12 @@ export function Main({ levels }: MainProps) {
                 </Tabs>
             </AppBar>
             <Box padding={2}>
-                <Stack direction={'row'} spacing={2}>
+                <Stack direction='row' spacing={2}>
                     <CodeEditor />
                     <Examples level={levels[currentLevel]} />
                 </Stack>
-                <Resources />
             </Box>
+            <ResourcesFab />
         </Box>
     )
 }
@@ -110,67 +109,5 @@ function CodeEditor() {
                 }}
             />
         </Box>
-    )
-}
-
-function Resources() {
-    const base = 'https://json-schema.org/understanding-json-schema/reference/'
-    const links = [
-        {
-            title: 'string',
-            slug: 'string'
-        },
-        {
-            title: 'RegEx',
-            slug: 'regular_expressions'
-        },
-        {
-            title: 'numbers',
-            slug: 'numeric'
-        },
-        {
-            title: 'object',
-            slug: 'object'
-        },
-        {
-            title: 'array',
-            slug: 'array'
-        },
-        {
-            title: 'boolean',
-            slug: 'boolean'
-        },
-        {
-            title: 'null',
-            slug: 'null'
-        },
-        {
-            title: 'Enumerated Values (enum)',
-            slug: 'enum'
-        },
-        {
-            title: 'Schema Composition',
-            slug: 'combining'
-        }
-    ]
-    return (
-        <Card elevation={2} sx={{ padding: 2 }}>
-            <Typography
-                gutterBottom
-                sx={{ color: 'text.secondary', fontSize: 14 }}
-            >
-                You can always use the
-                <Link sx={{ paddingLeft: 1 }} href={base}>
-                    JSON schema reference
-                </Link>
-            </Typography>
-            <List>
-                {links.map(({ title, slug }) => (
-                    <ListItem key={slug}>
-                        <Link href={`${base}${slug}`}>{title}</Link>
-                    </ListItem>
-                ))}
-            </List>
-        </Card>
     )
 }
