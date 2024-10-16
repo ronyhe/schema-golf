ef94f5a65338f7a25dcb2d9cd481ee5c4896c30d
ronyhe <ronyheruth@wix.com>
2024-10-16 16:40:01 +0300

    refactor

--- app/index.tsx
+++ app/index.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { Level, Main } from '#comps/Main'
 import { createRoot } from 'react-dom/client'
 import { CssBaseline } from '@mui/material'
+import { NonEmptyArray } from '#logic/utils'
 import examples from './levels.json'
 import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
-import { NonEmptyArray } from '#logic/utils'
 
 document.addEventListener('DOMContentLoaded', main)
 
