794ebb40b754b2500e80af880a2cc7d67d2a9750
ronyhe <ronyheruth@wix.com>
2024-10-17 20:21:10 +0300

    LOL! Phrasing!

--- test/components/Main.test.tsx
+++ test/components/Main.test.tsx
@@ -7,20 +7,7 @@ import { NonEmptyArray } from '#logic/utils'
 test('<Main />', async t => {
     t.afterEach(cleanup)
 
-    await t.test('shows the first level by default', () => {
-        const level = {
-            valid: ['This is super duper valid'],
-            invalid: ['Big mistake... Huge']
-        }
-        const { screen } = render(<Main levels={[level]} />)
-        const allExamples = [...level.valid, ...level.invalid]
-        for (const example of allExamples) {
-            const exact = false // Stringification slightly changes the output (quotes around strings for example)
-            expect(screen.getByText(example, { exact })).toBeInTheDocument()
-        }
-    })
-
-    await t.test('should list levels', () => {
+    await t.test('lists levels', () => {
         const levels = [
             {
                 valid: [1],
@@ -35,6 +22,19 @@ test('<Main />', async t => {
         const { screen } = render(<Main levels={levels} />)
         for (let i = 0; i < levels.length; i++) {
             expect(screen.getByText(`Level ${i + 1}`)).toBeInTheDocument()
+        }
+    })
+
+    await t.test('shows examples from the first level by default', () => {
+        const level = {
+            valid: ['This is super duper valid'],
+            invalid: ['Big mistake... Huge']
+        }
+        const { screen } = render(<Main levels={[level]} />)
+        const allExamples = [...level.valid, ...level.invalid]
+        for (const example of allExamples) {
+            const exact = false // Stringification slightly changes the output (quotes around strings for example)
+            expect(screen.getByText(example, { exact })).toBeInTheDocument()
         }
     })
 })
